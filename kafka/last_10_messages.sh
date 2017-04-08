3839f1320f49fd68a7b69b9fe34e3b107b4ff3a5
zhanghao <1891934@qq.com>
2017-04-09 00:06:51 +0800

    added limit

--- kafka/last_10_messages.sh
+++ kafka/last_10_messages.sh
@@ -1,3 +1,3 @@
 #在控制台里显示最新的10条消息
 #FIXME
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --max-messages 10 --topic test
