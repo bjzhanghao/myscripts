db6733ddf26dd4ad0adc11c24fa9b771b72f8595
zhanghao <1891934@qq.com>
2017-04-26 23:58:26 +0800

    added docker commands

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,4 +1,8 @@
 #常用docker命令
 
-#下载一个镜像
+#下载一个镜像(image)
 docker pull mydomain.com/myimage
+
+#启动一个容器(container)
+#其中--name指定容器的名字，-p映射端口（新:旧），-e环境变量
+docker run -tid --name iwind-db -p 3306:3306 -e MYSQL_DATABASE='mydb' myimage
