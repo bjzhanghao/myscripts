3a6ab3988b643164e27acf323e6cdf1f40855263
zhanghao <1891934@qq.com>
2017-04-28 19:33:21 +0800

    modified cp command

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -4,5 +4,9 @@
 #其中my-container是my.yml文件里的顶层node
 docker-compose -f my.yml up -d my-container
 
-#从宿主机复制文件到容器
+#从容器复制文件到宿主机
 docker cp <containerId>:/file/path/within/container /host/path/target
+
+#从宿主机复制文件到容器
+#其中deviceId需要docker inspect命令获取，在GraphDriver/Data/DeviceName里
+cp ./my.jar /var/lib/docker/devicemapper/mnt/<deviceId>/rootfs/tmp
