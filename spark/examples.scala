1eabc51c4237dac5a4a43b81e4a2a98895f77a7f
zhanghao <1891934@qq.com>
2017-05-12 11:59:47 +0800

    added list usages

--- spark/examples.scala
+++ spark/examples.scala
@@ -1,9 +1,12 @@
 //常用scala语法
 
-//构造一个List
+//List
 List(1,2,3)
 List.range(1,10)
 List.range(1,10,2)
+list1.length //长度
+list1 ::: list2 //连接两个list
+3 :: list1 //连接一个元素和一个list
 
 //构造一个array
 Array(1,2,3)
