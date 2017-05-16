8c346cadc26ab2e66a76e32b29c0ba3085ca0a5a
zhanghao <1891934@qq.com>
2017-05-16 23:43:03 +0800

    added Set and Tuple usages

--- spark/examples.scala
+++ spark/examples.scala
@@ -1,5 +1,9 @@
 //常用scala语法
 
+//构造一个array
+Array(1,2,3)
+Array.range(1,10)
+
 //List
 List(1,2,3)
 List.range(1,10)
@@ -8,6 +12,10 @@ list1.length //长度
 list1 ::: list2 //连接两个list
 3 :: list1 //连接一个元素和一个list
 
-//构造一个array
-Array(1,2,3)
-Array.range(1,10)
+//构造一个Set
+Set(1,2,3)
+
+//构造一个Tuple
+(3,5)
+(3,5)._1 //取key
+(3,5)._2 //取value
