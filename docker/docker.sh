e3a6cef70c597e79a2ce4c2b49e027a3cad327e8
zhanghao <1891934@qq.com>
2017-05-17 22:22:27 +0800

    empty

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,7 @@
 #常用docker命令
 
 #下载一个镜像(image)
-docker pull mydomain.com/myimage
+docker pull mydomain.com/myimage:mytag
 
 #启动一个容器(container)
 #其中--name指定容器的名字，-p映射端口（新:旧），-e环境变量
--- spark/params.sh
+++ spark/params.sh
@@ -5,3 +5,7 @@
 
 --num-executors executor的个数（仅在Yarn上运行时可用）
 --executor-memory 每个executor的可用内存，缺省值1G
+
+
+#实例
+spark2-shell --num-executors 10 --executor-memory 8G --jars "./KmxService.jar,/opt/cloudera/parcels/CDH/lib/hbase/hbase-client-1.2.0-cdh5.9.0.jar,/opt/cloudera/parcels/CDH/lib/hbase/hbase-common-1.2.0-cdh5.9.0.jar" --driver-class-path "/opt/cloudera/parcels/CDH/lib/hbase/hbase-client-1.2.0-cdh5.9.0.jar:/opt/cloudera/parcels/CDH/lib/hbase/hbase-common-1.2.0-cdh5.9.0.jar"
