b544b392d6c6c948904085668a1ffac8c3bd9522
zhanghao <1891934@qq.com>
2017-07-13 16:51:03 +0800

    Added comments

--- mysql/mysql-dump-timely.sh
+++ mysql/mysql-dump-timely.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#定期备份数据库并压缩，文件名包含备份时间
 NOW=$(date +'%Y%m%d_%H%M%S')
 mysqldump -uroot mydb|gzip> /root/backup/mydb_db_$NOW.sql.gz
--- mysql/mysql-dump-without-table.sh
+++ mysql/mysql-dump-without-table.sh
@@ -1,1 +1,2 @@
+#备份数据库（排除指定表格）
 mysqldump -uroot mydb --ignore-table=mydb.mytable|gzip> /root/backup/mydb_backup.sql.gz
