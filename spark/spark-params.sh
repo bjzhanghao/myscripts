fda1ceffca6b26edde9ea034602a645f7b50b1f0
zhanghao <1891934@qq.com>
2017-07-24 18:38:43 +0800

    Initial commit

--- spark/spark-params.sh
+++ spark/spark-params.sh
@@ -1,11 +1,11 @@
-#Spark命令行的常用参数
-#使用spark-shell --help可以查看所有可用参数
-#参数的使用方法举例：
-#spark-shell --executor-memory 8M
+//Spark命令行的常用参数
+//使用spark-shell --help可以查看所有可用参数
+//参数的使用方法举例：
+//spark-shell --executor-memory 8M
 
 --num-executors executor的个数（仅在Yarn上运行时可用）
 --executor-memory 每个executor的可用内存，缺省值1G
 
 
-#实例
+//实例
 spark2-shell --num-executors 10 --executor-memory 8G --jars "./KmxService.jar,/opt/cloudera/parcels/CDH/lib/hbase/hbase-client-1.2.0-cdh5.9.0.jar,/opt/cloudera/parcels/CDH/lib/hbase/hbase-common-1.2.0-cdh5.9.0.jar" --driver-class-path "/opt/cloudera/parcels/CDH/lib/hbase/hbase-client-1.2.0-cdh5.9.0.jar:/opt/cloudera/parcels/CDH/lib/hbase/hbase-common-1.2.0-cdh5.9.0.jar"
--- spark/spark-read-text-file.sh
+++ spark/spark-read-text-file.sh
@@ -1,27 +1,27 @@
-#spark-shell常用操作
+//spark-shell常用操作
 
-#读取一个文件
-#文件可以是hdfs上的相对路径，例如/my/file
-#得到的结果类型是RDD
+//读取一个文件
+//文件可以是hdfs上的相对路径，例如/my/file
+//得到的结果类型是RDD
 val file1 = sc.textFile("/my/file")
 
-#统计文件行数
+//统计文件行数
 file1.count()
 
-#将RDD转换为CSV比较麻烦
-#参考http://dblab.xmu.edu.cn/blog/1360-2/
+//将RDD转换为CSV比较麻烦
+//参考http://dblab.xmu.edu.cn/blog/1360-2/
 
-#也可以直接按csv格式读文件
-#得到的就是DataFrame类型（但不会自动将第一行认作表头）
+//也可以直接按csv格式读文件
+//得到的就是DataFrame类型（但不会自动将第一行认作表头）
 val file2 = spark.read.format("csv").load("/my/file")
 
-#另一种方式是使用spark-csv
-#https://github.com/databricks/spark-csv
-#其中spark是spark-shell内置的sparkSession对象
+//另一种方式是使用spark-csv
+//https://github.com/databricks/spark-csv
+//其中spark是spark-shell内置的sparkSession对象
 bin/spark2-shell --packages com.databricks:spark-csv_2.11:1.5.0
 val record = spark.read().format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load(userFile);
 
-#在spark2.0里
-#其中spark是spark-shell内置的sparkSession对象
-#参考http://stackoverflow.com/questions/29704333/spark-how-to-read-csv-file-as-dataframe
+//在spark2.0里（建议）
+//其中spark是spark-shell内置的sparkSession对象
+//参考http://stackoverflow.com/questions/29704333/spark-how-to-read-csv-file-as-dataframe
 val file3 = spark.read.option("header","true").csv(path)
