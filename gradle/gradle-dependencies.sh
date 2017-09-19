281065243ffb0ddb7efea5aac6c3be314d49f16c
zhanghao <1891934@qq.com>
2017-09-19 19:50:16 +0800

    Added gradle dependencies script

---
+++ gradle/gradle-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#查看项目依赖关系树
+#参考 https://stackoverflow.com/questions/20989317/multiple-dex-files-define-landroid-support-v4-accessibilityservice-accessibility
+gradle -q :mymodule:dependencies
