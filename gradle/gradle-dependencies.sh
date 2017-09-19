ce44c08f116ab41a601885519b83c6fcc47db0ca
zhanghao <1891934@qq.com>
2017-09-19 20:00:33 +0800

    Added comments

--- gradle/gradle-dependencies.sh
+++ gradle/gradle-dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #查看项目依赖关系树
 #参考 https://stackoverflow.com/questions/20989317/multiple-dex-files-define-landroid-support-v4-accessibilityservice-accessibility
-gradle -q :mymodule:dependencies
+#参考 http://www.mobibrw.com/2016/3777
+gradle -q mymodule:dependencies
+
+#结果中星号标记的项表示忽略，即不下载
