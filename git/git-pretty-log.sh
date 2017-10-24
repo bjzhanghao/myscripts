f515a5c6922d32a49e35c9c36c5d8ee104c0043f
zhanghao <1891934@qq.com>
2017-10-24 23:14:18 +0800

    added git pretty log

---
+++ git/git-pretty-log.sh
@@ -0,0 +1,15 @@
+#在命令行里以更易读的方式显示log
+git log --oneline --graph --decorate
+
+
+* 07b9804 (HEAD, origin/master, origin/HEAD, master) Added git replace branch script
+* fdf624e Initial commit
+* dc6be86 Added shell xargs
+* b1193fb Added shell find
+* a1922be Added shell history
+* 403f009 Added shell apropos
+* ce44c08 Added comments
+* 2810652 Added gradle dependencies script
+* 7180011 Removed unecessary files
+* 0d3fe4f Minor modifications
+
