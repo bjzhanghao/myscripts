165c016da801bf53c3fdad4a1e4e14f410d0b404
zhanghao <1891934@qq.com>
2018-02-02 19:02:23 +0800

    Added git-remote script

---
+++ git/git-remote.sh
@@ -0,0 +1,7 @@
+#更换远程repository地址（更换git库时有用）
+git remote set-url origin http://username@mydomain.com/scm/myproject.git
+
+#查看当前repository地址
+git remote -v
+#origin  http://username@mydomain.com/scm/myproject.git (fetch)
+#origin  http://username@mydomain.com/scm/myproject.git (push)
