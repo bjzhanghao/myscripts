cbee52bf9222044a037cf45381a6349311f075fe
zhanghao <1891934@qq.com>
2018-02-26 16:05:37 +0800

    Added an example

--- git/git-replace-branch.sh
+++ git/git-replace-branch.sh
@@ -3,3 +3,11 @@
 git checkout branch2
 git reset --hard branch1
 git push -f origin branch2
+
+
+#举例：
+#用分支dev的内容完全替换另一个分支master
+#替换完成后，两个分支内容都是原来dev的
+git checkout master
+git reset --hard dev
+git push -f origin master
