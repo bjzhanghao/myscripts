10594f8ba664890214888fe4413c4604581fe899
zhanghao <1891934@qq.com>
2018-08-10 14:15:07 +0800

    Added merge -s ours command

--- git/git-replace-branch.sh
+++ git/git-replace-branch.sh
@@ -11,3 +11,13 @@ git push -f origin branch2
 git checkout master
 git reset --hard dev
 git push -f origin master
+
+
+#另一种方法是用ours策略做merge
+#适用于当没有权限直接push -f的时候（即必须通过pull request的时候）
+git checkout dev
+git checkout -b dev_2
+git merge -s ours master
+git push origin dev_2
+#此时提交一个PR将dev_2合并到dev
+#上面的PR合并后，再提交一个PR将dev合并到master就不会有冲突了
