a9b9236a2ef2984c2d8ac7665912bebbdcbf01b3
zhanghao <1891934@qq.com>
2018-11-05 13:49:28 +0800

    Added mysqlshow

--- mysql/mysql-table-size.sql
+++ mysql/mysql-table-size.sql
@@ -1,6 +1,6 @@
 ﻿#使用information_schema表（实际是视图）
-use information_schema;
-select table_schema, table_name, table_rows, data_length, index_length from tables;
+select table_schema, table_name, table_rows, data_length, index_length from information_schema.tables;
 
 #或使用mysqlshow命令
-mysqlshow --status mydb mytable
+#表名是可选项
+mysqlshow --status mydb [mytable]
---
+++ mysql/mysqlshow.sh
@@ -0,0 +1,3 @@
+﻿#查看指定数据库指定表的空间使用情况
+#表名是可选项
+mysqlshow --status mydb [mytable]
