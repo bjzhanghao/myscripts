2ac37a583c4df4d699e7370235b1c18156e8160a
zhanghao <1891934@qq.com>
2019-02-19 13:40:02 +0800

    Revised scp command

--- shell/shell-scp.sh
+++ shell/shell-scp.sh
@@ -1,5 +1,8 @@
 #从本地复制文件到远程
-scp local_file remote_username@remote_ip:remote_folder
+scp local_file remote_username@remote_ip:remote_file
 
 #从远程复制文件到本地
-scp remote_username@remote_ip:remote_folder local_file
+scp remote_username@remote_ip:remote_file local_file
+
+#从远程复制文件到本地（文件夹）
+scp -r remote_username@remote_ip:remote_folder local_folder
