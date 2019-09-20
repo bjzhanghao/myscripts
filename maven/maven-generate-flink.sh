a5f5125e49ee2cf1555afb19236e807200795e87
zhanghao <1891934@qq.com>
2019-09-20 11:00:56 +0800

    Added generate flink project command

---
+++ maven/maven-generate-flink.sh
@@ -0,0 +1,2 @@
+#生成flink项目框架
+mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-java -DarchetypeVersion=1.9.0 -DgroupId=com.test -DartifactId=flink -Dversion=1.0.0 -Dpackage=com.test -DinteractiveMode=false
