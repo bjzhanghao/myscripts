23d6c822d1e8f5a8c671164f5c481ada9c2228c7
zhanghao <1891934@qq.com>
2019-09-23 12:29:26 +0800

    added kafka 2.3 console consumer command

--- kafka/kafka-console-consumer.sh
+++ kafka/kafka-console-consumer.sh
@@ -1,2 +1,5 @@
 #打印指定topic内容到控制台（并过滤）
 bin/kafka-console-consumer.sh --zookeeper 10.1.235.15 --topic defaultUser-defaultSpace-exception | grep '\[EXCEPTION\]'
+
+#kafka 2.3版本参数有所改变：
+bin/kafka-console-consumer.sh --bootstrap-server <localhost:9092> --topic <mytopic>
