1fb45feb01b3a1363ae7052cb95cf8488b3af7f9
zhanghao <1891934@qq.com>
2019-10-31 12:17:40 +0800

    Improved switch repository script

--- git/git-remote.sh
+++ git/git-remote.sh
@@ -1,10 +1,11 @@
-#更换远程repository地址（更换git库时有用）
-git remote set-url origin http://username@mydomain.com/scm/myproject.git
-
-git push -u origin --all
-git push origin --tags
-
 #查看当前repository地址
 git remote -v
 #origin  http://username@mydomain.com/scm/myproject.git (fetch)
 #origin  http://username@mydomain.com/scm/myproject.git (push)
+
+#更换远程repository地址（更换git库时有用）
+cd existing_repo
+git remote rename origin old-origin (可选)
+git remote add origin https://host/myproject/myproject.git
+git push -u origin --all
+git push -u origin --tags
